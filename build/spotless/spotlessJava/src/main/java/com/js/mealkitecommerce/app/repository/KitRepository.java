19c617884c081ad0b7334d7fb2f7b72ff3d9ff5f
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2023-11-27 21:39:51 +0900

    delete build package

--- build/spotless/spotlessJava/src/main/java/com/js/mealkitecommerce/app/repository/KitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.js.mealkitecommerce.app.repository;
-
-import com.js.mealkitecommerce.app.entity.Kit;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KitRepository extends JpaRepository<Kit, Long> {
-    List<Kit> findAllByOrderByIdDesc();
-}
