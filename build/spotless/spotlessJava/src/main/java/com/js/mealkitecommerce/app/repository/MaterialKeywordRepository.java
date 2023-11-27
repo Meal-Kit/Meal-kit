fd92e1591db9dc9dce416166e07e5cd5f1b88ed8
waitzero <magic2eye3@gmail.com>
2023-11-27 21:45:52 +0900

    delete:build삭제

--- build/spotless/spotlessJava/src/main/java/com/js/mealkitecommerce/app/repository/MaterialKeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.js.mealkitecommerce.app.repository;
-
-import com.js.mealkitecommerce.app.entity.material.MaterialKeyword;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialKeywordRepository extends JpaRepository<MaterialKeyword, Long> {
-    Optional<MaterialKeyword> findByContent(String keywordContent);
-}
